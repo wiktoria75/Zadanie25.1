ec1413ebca865076ebcb1e246a2b09c7f50525ed
Wiktoria Kulczyńska <wiktoriakulczynska@gmail.com>
2022-01-12 19:52:02 +0100

    Rozpoczęcie projektu

---
+++ src/main/java/wk/pl/zadanie25/Zadanie25Application.java
@@ -0,0 +1,13 @@
+package wk.pl.zadanie25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie25Application.class, args);
+    }
+
+}
---
+++ src/test/java/wk/pl/zadanie25/Zadanie25ApplicationTests.java
@@ -0,0 +1,13 @@
+package wk.pl.zadanie25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
